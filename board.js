530801ed5ec8e7956093d48694089c14d06e9276
daavinskiy <1avdeev.daniil@gmail.com>
2025-11-24 11:44:35 +0300

    fix: käytä _handleria removeEventListenerissä

--- board.js
+++ board.js
@@ -25,8 +25,10 @@ export function createBoard(cardCount) {
 
     cards.forEach(card => {
         const cardElement = createCardElement(card);
+
         cardElement._handler = () => handleCardFlip(cardElement);
         cardElement.addEventListener('click', cardElement._handler);
+
         gameBoard.appendChild(cardElement);
     });
 }
